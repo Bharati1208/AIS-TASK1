82379f6eb21c28aa8015184aa82b5a0fc3366740
Bharati1208 <144263200+Bharati1208@users.noreply.github.com>
2023-09-13 00:28:52 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -7,6 +7,13 @@ while num != 0:
     num= int(input("Enter another number( or 0 to exit):"))
     print("goodbye!")
 
+output:Enter a number:55
+55 is odd
+Enter another number( or 0 to exit):0
+goodbye!
+
+
+
 numbers_list = []
 i = 1
 while i <=5:
@@ -16,6 +23,14 @@ while i <=5:
         j += 1
     print() 
     i += 1
+
+
+output:
+*
+* *
+* * *
+* * * *
+* * * * *
     
     
     
@@ -26,16 +41,19 @@ while num <= 20:
     num += 1
 print(numbers_list)
 
+
+output:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
  
  
- 
-numbers = []
+ numbers = []
 num = 20
 while num >= 1:
     numbers.append(num)
     num -= 1
 print(numbers)
 
+output:[20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 
 
@@ -49,6 +67,17 @@ for i in range(1, 6):
     if i == 3:
         continue  
     print(i)
+
+output:1
+2
+1
+2
+4
+5
+1
+2
+4
+5
 
 
 for i in range(1, 6):
